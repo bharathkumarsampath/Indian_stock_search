0edbdc87d5549051b2f5b4d3f2bed9cac1f42c25
sampath bharath kumar <bharaths223@gmail.com>
2021-12-19 16:00:47 +0530

    added coding indentation

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -4,43 +4,99 @@ import ArrowDropDownIcon from  './Images/stockdown.png';
 import Add from  './Images/add.jpg';
 import Delete from  './Images/delete.jpg';
 
-export default function ({data,addToWatch}) {
-  const stockColor = data[1] < data[2] ? "#e45c4c":"#14d0bf"
-  const arrow = data[1] < data[2] ? ArrowDropDownIcon : ArrowDropUpIcon
-  const [style, setStyle] = useState('hidden');
-  const [backgroundColor,setBackgroundCOlor] = useState('#ffffff')
+export default function ({data,editWatchList}) {
+  
+  const [actionVisibility, setActionVisibility] = useState('hidden');
+  const [backgroundColor,setBackgroundColor] = useState('#ffffff')
+
+  const [stockName, priceOld,priceNew,inWatchList] = data.length?data:['Unknown',0,0,false]
+  const stockEditedName = stockName.substr(0,stockName.length - 5)
+  const stockPriceRounded = priceNew && priceNew.toFixed(2)
+  const stockExchangeName = stockName.substr(stockName.length - 3)
+  const percentageChange = (((priceOld-priceNew)/priceOld)*100).toFixed(2)+'%'
+  const stockColor = priceOld < priceNew ? "#e45c4c":"#14d0bf"
+  const arrow = priceOld < priceNew ? ArrowDropDownIcon : ArrowDropUpIcon
+
+  const style = {
+    root:{
+      display:'flex',
+      justifyContent:"space-between",
+      width:'400px',
+      marginBottom:'-25px',
+      borderTop:"1px solid",
+      borderTopColor:"rgb(235,229,229,0.86)",
+      paddingTop:"10px"
+    },
+    stockName:{
+      color:stockColor,
+      fontWeight:600,
+      marginTop:"100px",
+      fontSize:20,
+      marginBottom:'-25px'
+    },
+    stockPrice:{
+      color:stockColor,
+      fontWeight:600,
+      marginTop:"100px",
+      fontSize:20,
+      marginBottom:'-25px'
+    },
+    extraneous:{
+      display:'flex',
+      justifyContent:"space-between",
+      width:'400px',
+      paddingTop:'34px'
+    },
+    stockExchange:{
+      color:"#afafaf",
+      backgroundColor:'#f1f1f1',
+      padding:'5px',
+      width:'37px',
+      borderRadius:"4px"
+    },
+    action:{
+      cursor:'pointer',
+      visibility:actionVisibility
+    },
+    actionButton:{
+      marginBottom:'-5px'
+    },
+    percentage:{
+      color:'black'
+    }
+  }
 
   return data.length?(
-        <div style={{backgroundColor:backgroundColor}} onMouseEnter={e => {
-          setStyle('visible');setBackgroundCOlor('#f1f1f1')}}
-          onMouseLeave={e => {
-            setStyle( 'hidden');setBackgroundCOlor('#ffffff')
-        }}>
-        <div style={{display:'flex',justifyContent:"space-between",width:'400px',marginBottom:'-25px',borderTop:"1px solid",
-                 borderTopColor:"rgb(235,229,229,0.86)", paddingTop:"10px"}}
-                 >
+      <div style={{backgroundColor}} 
+             onMouseEnter={e => {
+               setActionVisibility('visible');
+               setBackgroundColor('#f1f1f1');}}
+             onMouseLeave={e => {
+               setActionVisibility('hidden');
+               setBackgroundColor('#ffffff');}}>
+        <div style={style.root}>
           <span>
-            <text style={{color:stockColor,fontWeight:600,marginTop:"100px",fontSize:20,marginBottom:'-25px'}}>{ data[0].substr(0,data[0].length - 5)}</text>
+            <text style={style.stockName}>{stockEditedName }</text>
           </span>
           <span>
-            <text style={{color:stockColor,fontWeight:600,marginTop:"100px",fontSize:20,marginBottom:'-25px'}}> {data[2] && data[2].toFixed(2)}</text>
-          </span>
-        </div>
-        <div style={{display:'flex',justifyContent:"space-between",width:'400px',paddingTop:'34px'}}
-          >
-          <span>
-            <text style={{color:"#afafaf",backgroundColor:'#f1f1f1',padding:'5px',width:'37px',borderRadius:"4px"}}>{data[0].substr(data[0].length - 3)}</text>
-          </span>
-          <div style={{cursor:'pointer',visibility:style}} onClick={()=>{addToWatch(data)}}>{data[3]?
-              <img   style={{marginBottom:'-5px'}} src={Delete} alt="fireSpot"/>:
-              <img   style={{marginBottom:'-5px'}} src={Add} alt="fireSpot"/>}</div>
-          <span>
-            <text style={{color:"black"}}>
-            <img   style={{marginBottom:'-5px'}} src={arrow} alt="fireSpot"/>{(((data[1]-data[2])/data[1])*100).toFixed(2)+'%'}</text>
+            <text style={style.stockPrice}> {stockPriceRounded}</text>
           </span>
         </div>
-        
+        <div style={style.extraneous}>
+          <span>
+            <text style={style.stockExchange}>{stockExchangeName}</text>
+          </span>
+          <div style={style.action} onClick={()=>{editWatchList(data)}}>{inWatchList?
+              <img   style={style.actionButton} src={Delete} alt="Delete Stock"/>:
+              <img   style={style.actionButton} src={Add} alt="Add Stock"/>}</div>
+          <span>
+            <text style={style.percentage}>
+              <img   style={style.actionButton} src={arrow} alt="ticker"/>
+              {percentageChange}
+            </text>
+          </span>
         </div>
+      </div>
   ):null
   
 }
